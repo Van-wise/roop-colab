e1c596c550241e9fd991ee116698f24204a0e8e5
Van-wise <fxmidiot@foxmail.com>
2024-11-02 17:41:59 +0800

    Update wise.py

--- wise.py
+++ wise.py
@@ -1,17 +1,17 @@
-# -- 下载模型 25s
+# -- 下载模型 26s
 import os
 import requests
 import zipfile
 from concurrent.futures import ThreadPoolExecutor
 
 models_info = [
-    ('https://github.com/karaokenerds/python-audio-separator/releases/download/v0.12.1/onnxruntime_gpu-1.17.0-cp310-cp310-linux_x86_64.whl', 'onnxruntime_gpu-1.17.0-cp310-cp310-linux_x86_64.whl', '/content/roop-colab/'),
-    ('https://huggingface.co/countfloyd/deepfake/resolve/main/inswapper_128.onnx', 'inswapper_128.onnx', '/content/roop-colab/checkpoints/'),
-    ('https://github.com/Hillobar/Rope/releases/download/Sapphire/inswapper_128.fp16.onnx', 'inswapper_128.fp16.onnx', '/content/roop-colab/checkpoints/'),
+    ('https://github.com/karaokenerds/python-audio-separator/releases/download/v0.12.1/onnxruntime_gpu-1.17.0-cp310-cp310-linux_x86_64.whl', 'onnxruntime_gpu-1.17.0-cp310-cp310-linux_x86_64.whl', '/content/roop/'),
+    ('https://huggingface.co/countfloyd/deepfake/resolve/main/inswapper_128.onnx', 'inswapper_128.onnx', '/content/roop/checkpoints/'),
+    ('https://github.com/Hillobar/Rope/releases/download/Sapphire/inswapper_128.fp16.onnx', 'inswapper_128.fp16.onnx', '/content/roop/checkpoints/'),
     ('https://github.com/deepinsight/insightface/releases/download/v0.7/buffalo_l.zip', 'buffalo_l.zip', '/content/'),
-    ('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth', 'GFPGANv1.4.pth', '/content/roop-colab/models/'),
-    ('https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth', 'detection_Resnet50_Final.pth', '/content/roop-colab/gfpgan/weights/'),
-    ('https://github.com/xinntao/facexlib/releases/download/v0.2.2/parsing_parsenet.pth', 'parsing_parsenet.pth', '/content/roop-colab/gfpgan/weights/')
+    ('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth', 'GFPGANv1.4.pth', '/content/roop/models/'),
+    ('https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth', 'detection_Resnet50_Final.pth', '/content/roop/gfpgan/weights/'),
+    ('https://github.com/xinntao/facexlib/releases/download/v0.2.2/parsing_parsenet.pth', 'parsing_parsenet.pth', '/content/roop/gfpgan/weights/')
 ]
 
 def download_model(url, name, path):
@@ -26,7 +26,7 @@ def download_model(url, name, path):
                     f.write(chunk)
         print(f"{name} 下载成功!")
         if name == 'buffalo_l.zip':
-            extract_zip(local_path,"/content/roop-colab/checkpoints/models/buffalo_l")
+            extract_zip(local_path,"/content/roop/checkpoints/models/buffalo_l")
             print(f"{name} 解压成功!")
     except Exception as e:
         print(f"{name} 文件下载错误：{e}")
